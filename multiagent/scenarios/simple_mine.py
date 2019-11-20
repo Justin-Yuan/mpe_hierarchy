d1d29ee4d67e944948e8e23e94f701f922ecfb98
Justin Yuan <justin.zcyuan@gmail.com>
2019-11-19 22:27:10 -0500

    fixed scenario config caching

--- multiagent/scenarios/simple_mine.py
+++ multiagent/scenarios/simple_mine.py
@@ -7,6 +7,8 @@ class Scenario(BaseScenario):
 
     def make_world(self, **kwargs):
         self.before_make_world(**kwargs)
+        # cache kwargs in case needed in Env wrapper 
+        self.config = kwargs
 
         world = World()
         world.np_random = self.np_random
